56757c757d4dbec9afb3886e7d81ff1d10c32e64
Rosemary Li <liy396@gene.com>
2023-03-05 17:01:12 -0800

    loops

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String args[]) {
         String question = "Who is the Greatest Player at all times for soccer?";
-        String choiceOne = "A. C Ronald";
-        String choiceTwo = "B. Messi";
-        String choiceThree = "C. Neymar";
+        String choiceOne = "C Ronald";
+        String choiceTwo = "Messi";
+        String choiceThree = "Neymar";
 
         String correctAnswer = choiceTwo;
 
@@ -23,14 +23,21 @@ public class Main {
         Scanner input = new Scanner(System.in);
         String UserAnswer = input.next();
 
+        while (!UserAnswer.equals(correctAnswer)) {
+            System.out.println("Sorry you are wrong, try again!");
+            System.out.println("What is your answer?");
+            UserAnswer = input.next();
+        }
+        System.out.println("Congratulations!");
+
         // If the user's input matches the correctAnswer...
         // then the user is correct and we want to print out a congrats message to the user.
-        if (UserAnswer.equals(choiceTwo)) {
-            System.out.println("Congratulations! You made the right choice!");
-        } else {
-            System.out.println("Sorry you are wrong, the correct answer is: ");
-            System.out.println(choiceTwo);
-        }
+//        if (correctAnswer.equals(UserAnswer)) {
+//            System.out.println("Congratulations! You made the right choice!");
+//        } else {
+//            System.out.println("Sorry you are wrong, the correct answer is: ");
+//            System.out.println(choiceTwo);
+//        }
 
         // If the user's input does not match the correctAnswer...
         // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
