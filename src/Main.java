e3c5a846fd3b58e0fbb8abd62de5c4974cfc644b
Rosemary Li <liy396@gene.com>
2023-03-05 22:51:45 -0800

    debug

--- src/Main.java
+++ src/Main.java
@@ -20,11 +20,11 @@ public class Main {
         double height = input.nextDouble();
         while (height <= 0) {
             System.out.println("That's invalid. Please input the base of the triangle (in inches).");
-            base = input.nextDouble();
+            height = input.nextDouble();
         }
 
         double area = (base * height) / 2;
-        System.out.println("The area is " + height);
+        System.out.println("The area is " + area);
 
     }
 }
